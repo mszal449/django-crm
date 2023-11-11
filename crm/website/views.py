83d4b11ba41f753ff7b9817a5a3e5563605e1d38
mszal <m.szalasz@gmail.com>
2023-11-11 22:09:16 +0100

    Added registration system

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    # Check if the request is a POST request
+    if request.method == "POST":
+        # If it is, try to log in
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error logging in, please try again.')
+            return redirect('home')
+    else:
+
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+
+
+
+
+
+
