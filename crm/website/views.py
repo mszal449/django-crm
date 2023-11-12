3d97d2a7c7bc1819940bf6b19a2933cd8c0b8cca
mszal <m.szalasz@gmail.com>
2023-11-12 18:11:07 +0100

    Added database mode, record table and record preview sites.

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     # Check if the request is a POST request
     if request.method == "POST":
         # If it is, try to log in
@@ -20,8 +23,7 @@ def home(request):
             messages.error(request, 'There was an error logging in, please try again.')
             return redirect('home')
     else:
-
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -31,12 +33,30 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
 
 
 
-
-
-
-
-
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page.")
+        return redirect('home')
