1c25989317663e4d359f79b7d3dbb8fb8a4bd9d7
mszal <m.szalasz@gmail.com>
2023-11-15 22:03:03 +0100

    Added a possibility to create a record.

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -70,4 +70,18 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.error(request, "You do not have permission to execute this operation")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Recoed Added.")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in.")
         return redirect('home')
